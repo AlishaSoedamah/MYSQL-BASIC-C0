f8d1b2e61752af2523487048171951c5cabb905e
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2020-11-12 11:33:45 +0100

    ans

--- 01-Databases/taak01-database-import/antwoorden.sql
+++ 01-Databases/taak01-database-import/antwoorden.sql
@@ -1,1 +1,101 @@
 -- Taak01 - haal de tabel movies op:
+Velvet Buzzsaw
+1
+2019
+6
+Alita: Battle Angel
+2
+2019
+8
+Avengers: Endgame
+3
+2019
+0
+Polar
+4
+2019
+6
+Bohemian Rhapsody
+5
+2018
+8
+Overlord
+6
+2018
+7
+Glass
+7
+2019
+7
+Green Book
+8
+2018
+8
+Aquaman
+9
+2018
+7
+Extremely Wicked, Shockingly Evil, and Vile
+10
+2019
+8
+Child's Play
+11
+2019
+0
+The Favourite
+12
+2018
+8
+Captain Marvel
+13
+2019
+0
+A Star Is Born
+14
+2018
+8
+Incredibles 2
+15
+2018
+8
+Fast & Furious Presents: Hobbs & Shaw
+16
+2019
+0
+The Lego Movie 2: The Second Part
+17
+2019
+7
+Joker
+18
+2019
+0
+Mortal Engines
+19
+2018
+6
+Cold Pursuit
+20
+2019
+7
+How to Train Your Dragon: The Hidden World
+21
+2019
+8
+Once Upon a Time in Hollywood
+22
+2019
+0
+Widows
+23
+2018
+7
+Robin Hood
+24
+2018
+5
+Roma
+25
+2018
+8
