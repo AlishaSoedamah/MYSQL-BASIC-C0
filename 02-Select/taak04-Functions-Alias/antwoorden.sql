036051ce26a00f4a307c31b672cb95ce8f403ff7
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2020-11-19 11:40:27 +0100

    how do you do opd 4

--- 02-Select/taak04-Functions-Alias/antwoorden.sql
+++ 02-Select/taak04-Functions-Alias/antwoorden.sql
@@ -7,13 +7,14 @@ SELECT SUM(wage) AS som_loon_fcgroningen FROM `players` WHERE club = "FC Groning
 -- Opdracht 4
 
 -- Opdracht 5
-
+SELECT AVG(wage) AS avg_wage_ned FROM `players` WHERE nationality = "Netherlands"
 -- Opdracht 6 
-
+SELECT AVG(wage) AS avg_wage_twenty_yo FROM `players` WHERE age < 20
 -- Opdracht 7 
-
+SELECT AVG(wage) AS avg_wage_older_then_twenty FROM `players` WHERE age > 20
 -- Opdracht 8
-
+SELECT SUM(wage) AS iedereen_van_chelsea FROM `players` WHERE club = "Chelsea"
 -- Opdracht 9
-
+SELECT AVG(age) AS leeftijd_avg_van_everyone FROM `players` WHERE 1
 -- Opdracht 10
+SELECT club,wage,AVG(wage) AS leeftijd_avg_pay_everyone_and_normal_pay FROM `players` WHERE club = "Liverpool"
